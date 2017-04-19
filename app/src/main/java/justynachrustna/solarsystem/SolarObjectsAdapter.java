0e66aec29fe7c6497fc661b79c66fbba8aee5892
Justyna Chrustna <justyna.chrustna@gmail.com>
2017-04-19 23:21:40 +0200

    ViewHolder

--- app/src/main/java/justynachrustna/solarsystem/SolarObjectsAdapter.java
+++ app/src/main/java/justynachrustna/solarsystem/SolarObjectsAdapter.java
@@ -4,6 +4,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Justyna on 19.04.2017.
@@ -24,6 +31,8 @@ public class SolarObjectsAdapter extends RecyclerView.Adapter<SolarObjectsAdapte
 
     @Override
     public void onBindViewHolder(SolarObjectViewHolder holder, int position) {
+        SolarObject solarObject = solarObjects[position];
+        holder.setSolarObject(solarObject);
 
     }
 
@@ -33,8 +42,28 @@ public class SolarObjectsAdapter extends RecyclerView.Adapter<SolarObjectsAdapte
     }
 
     class SolarObjectViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.imageView)
+        ImageView itemImageView;
+        @BindView(R.id.itemTextView)
+        TextView itemTextView;
+
+        private SolarObject solarObject;
+
         public SolarObjectViewHolder (View itemView){
             super(itemView);
+            ButterKnife.bind(this. itemView);
+        }
+
+        public void setSolarObject(SolarObject solarObject) {
+            this.solarObject = solarObject;
+            itemTextView.setText(solarObject.getName());
+
+            Glide.with(itemImageView.getContext()).load("file:///android_asset/"+ solarObject.getImage()).into(itemImageView);
+
+        }
+
+        public SolarObject getSolarObject() {
+            return solarObject;
         }
     }
 }
